7793b6905986a86f70a476aede091782401b7ebb
Richa Maxx <richamaxx@Richas-MacBook-Air.local>
2023-10-09 14:11:37 -0700

    feat: added callbacks and higher order functions assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,15 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let result;
 
-// Using `forEach` function here
+function forEach(array, operation) {
+  let newEmployeeSalaries = []
+  for (let i = 0; i < array.length; i++) {
+    newEmployeeSalaries.push(array[i] + 5000);
+  }
+  return newEmployeeSalaries
+}
+result = forEach(employeeSalaries)
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(result);
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,14 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  const moreThan70 = arr.some(o => o === 70)
+  return (arr.length === 5 && moreThan70) || !moreThan70
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
-
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+console.log(atLeastFive(studentScoresRoom1))
+console.log(atLeastFive(studentScoresRoom2))
+console.log(atLeastFive(studentScoresRoom3))
