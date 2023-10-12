df1f9674b6209a218bdfc6fb478269c550f684de
Panida Chokbancha <PANIDCHO@cementhai.com>
2023-10-12 11:09:53 +0700

    Feat : add aom callback function assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,19 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
+
+function addSalary(currentSalaries) {
+  { return currentSalaries + 5000 }};
+
+function forEach( array, operation) { 
+  let newArray = [];
+  for ( let i = 0 ; i < array.length ; i++)
+  {newArray.push(operation(array[i]))}
+return newArray 
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
-
-// Using `forEach` function here
+const newEmployeeSalaries = forEach(employeeSalaries,addSalary);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
+
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,30 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
+function CheckSeventyUp(number)
+  { return number >= 70 }
+
+function atLeastFive(array, operation) 
+  { const greaterThanSeventy = array.filter(operation)
+   
+   return greaterThanSeventy.length >= 5 }
+
+   //function atLeastFive(array, operation)
+  //{ let greaterthanSeventy = [];
+  //for ( let i = 0 ; i < array.length ; i++)
+  // greaterThanSeventy = array[i].filter(operation)
+  // if ( greaterThanSeventy >= 5){ return true }
+  //else {false} } 
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+const test = [67, 80, 10,80];
 
-// Using `atLeastFive` function here
+let scoreRoom1Result = console.info(atLeastFive(studentScoresRoom1,CheckSeventyUp))
+let scoreRoom2Result = console.info(atLeastFive(studentScoresRoom2,CheckSeventyUp))
+let scoreRoom3Result = console.info(atLeastFive(studentScoresRoom3,CheckSeventyUp))
+let Result = console.info(atLeastFive(test,CheckSeventyUp))
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+
+
+
